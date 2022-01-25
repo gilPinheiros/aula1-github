81455c053163877893db647efe1113a8412e1ff9
gilPinheiros <gil.101dalmatas@hotmail.com>
2022-01-25 20:04:08 -0300

    Projeto criado

---
+++ src/application/teste.java
@@ -0,0 +1,10 @@
+package application;
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Teste realizado com sucesso");
+	}
+
+}
